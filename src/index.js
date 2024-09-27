9d7492b14ec0c53770dd4b0bef82e28c606858be
Kresna Satya <kresnasatya72@gmail.com>
2024-09-27 12:23:38 +0800

    rename schema and update script

--- src/index.js
+++ src/index.js
@@ -11,7 +11,25 @@
 export default {
 	async fetch(request, env, ctx) {
 		const url = new URL(request.url);
-		const { pathname } = url;
-		return Response.redirect(`https://github.com/kresnasatya${pathname}`, 301);
+		const { hostname, pathname } = url;
+		switch (hostname) {
+			case 'github.kresna.me':
+				return Response.redirect(`https://github.com/kresnasatya${pathname}`, 301);
+			case 'linkedin.kresna.me':
+				return Response.redirect(`https://linkedin.com/in/kresnasatya`, 301);
+			case 'youtube.kresna.me':
+				let redirectUrl = 'https://youtube.com/@kresnasatya';
+				switch (pathname) {
+					case '/skripsi-bion':
+						redirectUrl = 'https://youtu.be/dC9OSWHPi-M'
+						break;
+					default:
+						redirectUrl = redirectUrl;
+						break;
+				}
+				return Response.redirect(redirectUrl, 301);
+			default:
+				return Response.redirect(`https://kresna.me`, 301);
+		}
 	}
 };
